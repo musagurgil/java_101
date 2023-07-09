1d5040e90591dc29af57d159eb661d82384a677e
Musa Gürgil <34582038+musagurgil@users.noreply.github.com>
2023-07-10 01:19:23 +0300

    Add files via upload
    "Classes" isimli ogrenimDosyam eklenildi.

---
+++ ogrenimDosyam/classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Müşteri ekleme işlemi başarıyla tamamlandı.");
+    }
+
+    public void Remove(){
+        System.out.println("Müşteri silme işlemi başarıyla tamamlandı.");
+    }
+
+    public void Update(){
+        System.out.println("Müşteri güncelleme işlemi başarıyla tamamlandı.");
+    }
+}
