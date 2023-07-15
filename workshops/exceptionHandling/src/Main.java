a70958f661f7aa004067a88ccc11c8e0c9593904
Musa Gürgil <34582038+musagurgil@users.noreply.github.com>
2023-07-15 23:25:04 +0300

    Add files via upload
    "exceptionHandling" isimli ogrenimDosyam eklenildi.

---
+++ workshops/exceptionHandling/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] sayilar = new int[]{1, 2, 3,};
+            System.out.println(sayilar[5]);
+        } catch (StringIndexOutOfBoundsException exception) {
+            System.out.println("Hata oluştu : " + exception);
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Hata oluştu : " + exception);
+        }catch (Exception exception){
+            System.out.println("Loglandı : "+exception);
+        }finally {
+            System.out.println("Program tamamlandı");
+        }
+    }
+}
